b728c90ae3d1bda2315a91d2ca2541f3a7d2d860
D. Rimron-Soutter <d@occa-design.com>
2014-08-11 17:30:35 +0100

    Auto config builder...
    -Dx

---
+++ config/config.example.js
@@ -0,0 +1,16 @@
+module.exports = config = {
+    "name" : "Cimentarius",
+
+    "tracking_port" : 8080,
+    "tracking_address" : "127.0.0.1",
+	// Admin URL
+    "dashboard": ".admin",
+
+    // And the details for saving...
+    "dbUser" : "cimentarius",
+    "dbPass" : "you_secret_password",
+    "dbName" : "cimentarius_sitename",
+    "dbHost" : "localhost",
+    "dbPort" : 3306,
+
+};
