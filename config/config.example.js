4dd637bc77afa52839e5007e0903b74b6a94f7d1
D. Rimron <darran@xalior.com>
2014-08-20 00:00:25 +0100

    update examle to match newer current of dev

--- config/config.example.js
+++ config/config.example.js
@@ -1,9 +1,9 @@
 module.exports = config = {
     "name" : "Cimentarius",
 
-    "tracking_port" : 8080,
-    "tracking_address" : "127.0.0.1",
-	// Admin URL
+    "listen_port" : 8080,
+    "listen_address" : "127.0.0.1",
+    // Admin URL
     "dashboard": ".admin",
 
     // And the details for saving...
@@ -11,6 +11,5 @@ module.exports = config = {
     "dbPass" : "you_secret_password",
     "dbName" : "cimentarius_sitename",
     "dbHost" : "localhost",
-    "dbPort" : 3306,
-
+    "dbPort" : 3306
 };
