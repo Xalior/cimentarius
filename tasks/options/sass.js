56ae621b22854bb28df71e0746889168e39a986d
D. Rimron-Soutter <darran@xalior.com>
2014-09-13 19:01:42 +0100

    Sorting out grunt+compass+bootstrap. I hate this shit.
    -Dx

--- tasks/options/sass.js
+++ tasks/options/sass.js
@@ -2,7 +2,9 @@ module.exports = {
   dist: {
     options: {
       // cssmin will minify later
-      style: 'expanded'
+      style: 'expanded',
+      compass: true,
+      require: 'bootstrap-sass'
     },
     files: {
       'src/css/cimentarius.css': 'src/scss/cimentarius.scss'
