d9714991df8f8e87d963b53a1617a12d7520dc61
D. Rimron-Soutter <d@occa-design.com>
2014-09-18 17:01:38 +0100

    remove some debugging...
    -Dx

--- tasks/dev.js
+++ tasks/dev.js
@@ -1,7 +1,6 @@
 module.exports = function(grunt) {
     grunt.registerTask('dev', ['concat', 'sass', 'imagemin', 'autoprefixer', 'cssmin', 'copy']);
     grunt.registerTask('watch:dev', function() {
-        console.log(grunt.config.get(['watch']).dev);
         grunt.config('watch', grunt.config.get(['watch']).dev);
         grunt.task.run('watch');
     });
