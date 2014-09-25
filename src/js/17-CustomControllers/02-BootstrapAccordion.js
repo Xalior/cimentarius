05b80d15d5b3df6f398cf448207fa2a61ad1b8f3
D. Rimron-Soutter <darran@xalior.com>
2014-09-25 16:15:21 +0100

    Dependancies for Dashboard tree and folder, and controllers.
    Controllers are now maintained in files that relate the ng-app in question (eg BootstrapAccordion)

--- src/js/17-CustomControllers/02-BootstrapAccordion.js
+++ src/js/17-CustomControllers/02-BootstrapAccordion.js
@@ -1,26 +1,74 @@
-Cimentarius.controller('BootstrapAccordion', function($scope) {
+Cimentarius.controller('SiteOverviewAccordion', function ($scope, $timeout) {
+    $scope.init = function () {
+        $scope.reload();
+    };
+
+    $scope.reload = function() {
+//        $timeout(function () {
+            console.log("time up");
+            $scope.redraw([
+                {
+                    title: 'First Site - Fakey!',
+                    comment: 'Comment about shite',
+                    pages: [
+                        {
+                            "id": 1,
+                            "title": "1. dragon-breath",
+                            "pages": []
+                        },
+                        {
+                            "id": 2,
+                            "title": "2. moiré-vision",
+                            "pages": [
+                                {
+                                    "id": 21,
+                                    "title": "2.1. tofu-animation",
+                                    "pages": [
+                                        {
+                                            "id": 211,
+                                            "title": "2.1.1. spooky-giraffe",
+                                            "pages": []
+                                        },
+                                        {
+                                            "id": 212,
+                                            "title": "2.1.2. bubble-burst",
+                                            "pages": []
+                                        }
+                                    ]
+                                },
+                                {
+                                    "id": 22,
+                                    "title": "2.2. barehand-atomsplitting",
+                                    "pages": []
+                                }
+                            ]
+                        },
+                        {
+                            "id": 3,
+                            "title": "3. unicorn-zapper",
+                            "pages": []
+                        },
+                        {
+                            "id": 4,
+                            "title": "4. romantic-transclusion",
+                            "pages": []
+                        }]
+                },
+                {
+                    title: 'Dynamic Group Header - 2',
+                    comment: 'Dynamic Group Body - 2',
+                    pages: []
+                }
+            ]);
+//        }, 1000);
+    };
+
+    $scope.redraw = function(data) {
+        console.log(data);
+        $scope.sites = data;
+    }
+
     $scope.oneAtATime = true;
 
-    $scope.groups = [
-        {
-            title: 'Dynamic Group Header - 1',
-            content: 'Dynamic Group Body - 1'
-        },
-        {
-            title: 'Dynamic Group Header - 2',
-            content: 'Dynamic Group Body - 2'
-        }
-    ];
-
-    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-    $scope.addItem = function() {
-        var newItemNo = $scope.items.length + 1;
-        $scope.items.push('Item ' + newItemNo);
-    };
-
-    $scope.status = {
-        isFirstOpen: true,
-        isFirstDisabled: false
-    };
+    $scope.sites = [];
 });
