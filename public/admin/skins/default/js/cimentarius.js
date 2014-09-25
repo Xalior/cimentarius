a169c6ea44e02b2f4735c915865e294fd2b14eb2
D. Rimron-Soutter <darran@xalior.com>
2014-09-25 13:59:16 +0100

    Add support for Accordion...
    -dx

--- public/admin/skins/default/js/cimentarius.js
+++ public/admin/skins/default/js/cimentarius.js
@@ -26139,3 +26139,29 @@ Cimentarius.controller('BootstrapAlert', function($scope) {
         $scope.alerts.splice(index, 1);
     };
 });
+Cimentarius.controller('BootstrapAccordion', function($scope) {
+    $scope.oneAtATime = true;
+
+    $scope.groups = [
+        {
+            title: 'Dynamic Group Header - 1',
+            content: 'Dynamic Group Body - 1'
+        },
+        {
+            title: 'Dynamic Group Header - 2',
+            content: 'Dynamic Group Body - 2'
+        }
+    ];
+
+    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+    $scope.addItem = function() {
+        var newItemNo = $scope.items.length + 1;
+        $scope.items.push('Item ' + newItemNo);
+    };
+
+    $scope.status = {
+        isFirstOpen: true,
+        isFirstDisabled: false
+    };
+});
