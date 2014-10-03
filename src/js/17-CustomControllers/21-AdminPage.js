5b82f733087fe62c78504e1e6d5d8d0a322f3f5d
D. Rimron <darran@xalior.com>
2014-10-03 22:09:55 +0100

    Client and Server side validation done. Record saving done. Errors done. Errors over wire done. Error reporting on fields done.
    Next. Confirm record saving via metadata display.
    
    -Dx

--- src/js/17-CustomControllers/21-AdminPage.js
+++ src/js/17-CustomControllers/21-AdminPage.js
@@ -1,28 +1,51 @@
 Cimentarius.controller('pageController', ['$scope', '$http', '$location', function($scope, $http, $location) {
     $scope.submitted = false;
 
+    $scope.serverErrors = {};
+
     $scope.submit = function() {
-        console.log(pageForm);
         if ($scope.pageForm.$valid) {
-            console.log('valid');
             $scope.pageForm.submitted = true;
-            console.log($location);
             $http({
                 method: 'POST',
                 url: $location.$$absUrl,
                 data: $scope.page
             }).success(function(data, status, headers, cfg) {
-                console.log(data);
+                if(data.errors) {
+                    for (var key in data.errors) {
+                        var obj = data.errors[key];
+                        for (var prop in obj) {
+                            if(obj.hasOwnProperty(prop)){
+                                $scope.serverErrors[key] = obj[prop];
+                                $scope.pageForm[key].$setValidity('serverSide', false);
+                            }
+                        }
+                    }
+                } else {
+                    console.log('job done');
+                }
             }).error(function(data, status, headers, cfg) {
                 console.error(data);
             });
         } else {
             $scope.pageForm.submitted = false;
         }
-    }
+    };
+
+    $scope.change = function() {
+        for (var key in $scope.serverErrors) {
+            var obj = $scope.serverErrors[key];
+            for (var prop in obj) {
+                if (obj.hasOwnProperty(prop)) {
+                    $scope.serverErrors[key] = '';
+                    $scope.pageForm[key].$setValidity('serverSide', true);
+                }
+            }
+        }
+    };
 
     $scope.init = function(data) {
         $scope.page = data;
 
-    }
+    };
 }]);
