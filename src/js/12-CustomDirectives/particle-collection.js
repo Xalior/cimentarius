cb7d45a3626a69d2825eb26bff310d0386c42062
D. Rimron-Soutter <d@occa-design.com>
2014-10-28 12:57:09 +0000

    Sort CSS for Particle list, and icons, to make them 'less crap.'
    -Dx

--- src/js/12-CustomDirectives/particle-collection.js
+++ src/js/12-CustomDirectives/particle-collection.js
@@ -35,14 +35,17 @@ Cimentarius.directive('particleCollection', function () {
                 '           dnd-draggable="particle" dnd-moved="contentBlock.particles.splice($index, 1)" dnd-effect-allowed="move" dnd-selected="models.selected = particle" ' +
                 '           ng-class="{\'selected\': models.selected === particle}">' +
                 '            <div class="row">' +
-                '              <div class="glyphicon {{ particle.className }} particle-type col-sm-1 columns" tooltip="{{ particle.type}}"></div>' +
-                '              <div class="col-sm-7 columns">' +
+                '              <div class="col-sm-1 columns">' +
+                '                <div class="particle-type" tooltip="{{ particle.type}}" tooltip-placement="right">' +
+                '                  <span class="glyphicon {{ particle.className }}"></div>' +
+                '                </div>' +
+                '              <div class="col-sm-10 columns">' +
                 '                {{ particle.title }}' +
                 '                {{ particle.position }}' +
                 '              </div>' +
-                '              <div class="col-sm-2 columns" dropdown>' +
+                '              <div class="col-sm-1 columns" dropdown>' +
                 '                <button type="button" class="btn-xs btn-primary dropdown-toggle fullwidth">' +
-                '                  <small>More&hellip; <span class="caret"></span></small>' +
+                '                  <small><span class="caret"></span></small>' +
                 '                </button>' +
                 '                <ul class="dropdown-menu" role="menu" ng-model="contentBlock.types">' +
                 '                  <h6>New Content <strong>ABOVE</strong>&hellip;</h6>' +
@@ -61,8 +64,11 @@ Cimentarius.directive('particleCollection', function () {
                 '            </div>' +
                 '          </li>' +
                 '          <li ng-if="!contentBlock.particles.length" class="no-particles">' +
-                '            <div>' +
-                '              <span class="glyphicon glyphicon-warning-sign"></span>This content block is empty&hellip;<br /><em>Content blocks invariably look better when they contain content!</em>' +
+                '            <div class="row">' +
+                '              <div class="column col-sm-12 no-particles">' +
+                '                <span class="glyphicon glyphicon-warning-sign"></span>' +
+                '                <div>This content block is empty&hellip;<br /><em>Content blocks invariably look better when they contain content!</em></div>' +
+                '              </div>' +
                 '            </div>' +
                 '          </li>' +
                 '        </ul>' +
